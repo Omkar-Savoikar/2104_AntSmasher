88744369457e73caff3b5c4e903937d8e7db9186
Omkar-Savoikar <mca.2104@unigoa.ac.in>
2022-04-09 16:52:29 +0530

    Update scoreboard

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 var score = 0;
+var score_val = document.getElementsByClassName("val")[0];
+var ant = document.getElementsByClassName("ant")[0].getElementsByTagName("img")[0];
+var bee = document.getElementsByClassName("bee")[0].getElementsByTagName("img")[0];
 
 function startGame() {
-	// let x;
-	// if (document.getElementsByClassName("start").innerHTML == "Start")
-	// 	x = true;
-	// if (document.getElementsByClassName("start").innerHTML == "Stop")
-	// 	x = false;
-	// if(!x) {
-	// 	document.getElementsByClassName("pause")[0].style.display = "inline";
-	// 	document.getElementsByClassName("start")[0].innerHTML = "Stop";
-	// }
 	document.getElementsByClassName("pause")[0].style.display = "inline";
 	document.getElementsByClassName("start")[0].innerHTML = "Stop";
 }
 
+function updateScore() {
+	score += 100;
+	score_val.innerHTML = score;
+}
+
 function pauseGame() {
-	if (document.getElementsByClassName("pause")[0].innerHTML == "Pause")
-		document.getElementsByClassName("pause")[0].innerHTML = "Play";
-	else
-		document.getElementsByClassName("pause")[0].innerHTML = "Pause";
+	document.getElementsByClassName("pauseplay")[0].style.display = "block";
+}
+
+function ctnGame() {
+	document.getElementsByClassName("pauseplay")[0].style.display = "none";
 }
 
 function endGame() {
+	document.getElementsByClassName("pause")[0].style.display = "none";
+	document.getElementsByClassName("start")[0].innerHTML = "Start";
 	alert("Game ended. Your score is " + score);
 	score = 0;
+	score_val.innerHTML = score;
 }
+
+function game() {
+	if (document.getElementsByClassName("start")[0].innerHTML == "Start")
+		startGame();
+	else
+		endGame();
+}
+
+// ant.addEventListener("click", updateScore());
+// class Ant {
+//     constructor(x, y) {
+//         this.x = Math.floor(Math.random() * 421) + ((width-600)/2);
+//         this.y = 100;
+//     }
+//     width = screen.width;
+// }
