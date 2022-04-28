38e580bc178176b18d2fe245ca23d1f20a1530ed
Omkar-Savoikar <mca.2104@unigoa.ac.in>
2022-04-28 18:30:49 +0530

    Add features

--- index.js
+++ index.js
@@ -1,59 +1,132 @@
 var score = 0;
+var antCount = 0;
+var id;
+var setId = 0;
 var score_val = document.getElementsByClassName("val")[0];
-var ant = document.getElementsByClassName("ants")[0].getElementsByTagName("img")[0];
-var bee = document.getElementsByClassName("bees")[0].getElementsByTagName("img")[0];
-var anime = ['anim1', 'anim2', 'anim3', 'anim4', 'anim5']
+var rag = null;
+let width = ((window.innerWidth - 1000) / 2) + 200;
+const lefts = [width, width+84, width+168, width+252, width+338];
+
+function divGenerator(className) {
+    let setLeft = lefts[Math.floor(Math.random() * lefts.length)];
+    setLeft += "px";
+    let div = document.createElement("div");
+    div.setAttribute("class", className);
+    let temp = "div" + setId;
+    div.setAttribute("id", temp);
+    div.style.left = setLeft;
+    return div;
+}
+
+function imgGenerator(srcs) {
+    let img = document.createElement("img");
+    let temp = srcs + ".png";
+    img.src = temp;
+    img.alt = srcs;
+    temp = "img" + setId;
+    img.setAttribute("id", temp);
+    img.style.display = "inline";
+    setId++;
+    return img;
+}
+
+function antGenerator() {
+    if (antCount == 5) 
+        return;
+    antCount++;
+    let div = divGenerator("ants")
+    let img = imgGenerator("ant");
+    div.appendChild(img);
+    document.getElementsByClassName("board")[0].appendChild(div);
+}
+
+function beeGenerator() {
+    let div = divGenerator("bees")
+    let img = imgGenerator("bee");
+    div.appendChild(img);
+    document.getElementsByClassName("board")[0].appendChild(div);
+}
+
+function beeShow() {
+    let setLeft = lefts[Math.floor(Math.random() * lefts.length)];
+    setLeft += "px";
+    document.getElementsByClassName("bees")[0].style.left = setLeft;
+    document.getElementsByClassName("bees")[0].children[0].style.display = "inline";
+    setTimeout(beeHide, 4000);
+}
+
+function beeHide() {
+    document.getElementsByClassName("bees")[0].children[0].style.display = "none";
+    let hide = Math.floor(Math.random() * 5) + 1;
+    setTimeout(beeShow, hide * 4000);
+}
+
+function randomGenerator() {
+    let val = Math.floor(Math.random() * 10) + 2;
+    if (antCount == 0)
+        val = 0;
+    setTimeout(antGenerator, val * 1000);
+}
 
 function startGame() {
-	document.getElementsByClassName("pause")[0].style.display = "inline";
-	document.getElementsByClassName("start")[0].innerHTML = "Stop";
-	ant.setAttribute('class', anime[0]);
-	bee.setAttribute('class', anime[0]);
+    document.getElementsByClassName("start")[0].innerHTML = "Stop";
+    score_val.innerHTML = score;
+    beeGenerator();
+    beeHide();
 }
 
 function updateScore() {
-	score += 100;
-	score_val.innerHTML = score;
-}
-
-function pauseGame() {
-	ant.removeAttribute('class', 'run');
-	ant.setAttribute('class', 'pause');
-	document.getElementsByClassName("text")[0].innerHTML = "Game Paused. Press PLAY to continue playing.";
-	document.getElementsByClassName("ctn")[0].innerHTML = "PLAY";
-	document.getElementsByClassName("pauseplay")[0].style.display = "block";
+    let divId = document.getElementById(id).parentElement.id;
+    let div = document.getElementById(divId);
+    document.getElementById(divId).removeChild(div.firstChild);
+    antCount--;
+    score += 100;
+    score_val.innerHTML = score;
+    document.getElementById(divId).remove();
 }
 
 function ctnGame() {
-	ant.removeAttribute('class', 'pause');
-	ant.setAttribute('class', 'run');
-	document.getElementsByClassName("pauseplay")[0].style.display = "none";
+    document.getElementsByClassName("pauseplay")[0].style.display = "none";
+    setTimeout(location.reload(true));
+}
+
+function clearBoard() {
+    let board = document.getElementsByClassName("board")[0];
+    let childCount = board.childElementCount;
+    for (let i = 0; i < childCount; i++)
+        board.removeChild(board.children[0]);
 }
 
 function endGame() {
-	ant.removeAttribute('class', anime[0]);
-	bee.removeAttribute('class', anime[0]);
-	document.getElementsByClassName("pause")[0].style.display = "none";
-	document.getElementsByClassName("start")[0].innerHTML = "Start";
-	document.getElementsByClassName("text")[0].innerHTML = `Game ended. Your score is ${score}`;
-	document.getElementsByClassName("ctn")[0].innerHTML = "Restart";
-	document.getElementsByClassName("pauseplay")[0].style.display = "block";
-	score = 0;
-	score_val.innerHTML = score;
+    clearInterval(rag);
+    document.getElementsByClassName("start")[0].innerHTML = "Start";
+    document.getElementsByClassName("text")[0].innerHTML = 'Game ended. Your score is' + score;
+    document.getElementsByClassName("ctn")[0].innerHTML = "OK";
+    document.getElementsByClassName("pauseplay")[0].style.display = "block";
+    document.getElementsByClassName("bees")[0].getElementsByTagName("img")[0].style.display = "none";
+    score = 0;
+    score_val.innerHTML = score;
+    setTimeout(clearBoard, 1000);
 }
 
 function game() {
-	if (document.getElementsByClassName("start")[0].innerHTML == "Start")
-		startGame();
-	else
-		endGame();
+    if (document.getElementsByClassName("start")[0].innerHTML == "Start") {
+        rag = setInterval(randomGenerator, 2000);
+        startGame();
+    } else {
+        endGame();
+    }
 }
 
-// ant.addEventListener("click", updateScore());
-// class Ant {
-//     constructor(x, y) {
-//         this.x = Math.floor(Math.random() * 421) + ((width-600)/2);
-//         this.y = 100;
-//     }
-//     width = screen.width;
-// }
+window.onclick = (e) => {
+    if (document.getElementsByClassName("start")[0].innerHTML == "Start")
+        if (e.target.className != "start") return;
+    if (e.target.tagName.toLowerCase() != "img") return;
+    id = e.target.id;
+    if (e.target.alt.toLowerCase() == "ant") updateScore();
+    if (e.target.alt.toLowerCase() == "bee") endGame();
+};
+
+window.ondragstart = function () {
+    return false;
+};
